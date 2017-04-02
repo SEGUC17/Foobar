6ded1b26e986ce34f0122aecd1f7e218ced3456f
AliWaleed97 <ali.sobh@student.guc.edu.eg>
2017-04-02 12:07:51 +0200

    Reformat 'server.js'

--- server.js
+++ server.js
@@ -1,39 +1,34 @@
-//Require Dependencies
-var express = require('express');
-var router = require('./source/routes');
-var bodyParser = require('body-parser');
-var passport = require('passport');
-var flash = require('connect-flash');
-var morgan = require('morgan');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
+// Require Dependencies
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const flash = require('connect-flash');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const mongoose = require('mongoose');
 
-var mongoose = require('mongoose');
-var DB_URI = "mongodb://localhost:27017/portfolio";
+const app = express();
 
-var app = express();
-
-
-
-app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
 // Configure app
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
 
 // Required for Passport
 app.use(session({
   secret: 'ilovescotchscotchyscotchscotch',
   cookie: {
-    maxAge: 60000
-  }
+    maxAge: 60000,
+  },
 })); // session secret
+
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
@@ -41,21 +36,21 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 require('./source/routes.js')(app, passport);
 require('./source/config/passport')(passport);
 
+const DB_URI = 'mongodb://localhost:27017/portfolio';
 mongoose.connect(DB_URI);
 // app.use(router);
 
-//GlOBAL VARS
-app.use(function(req, res, next) {
-  // res.locals.success_msg = req.flash('success_msg');
-  // res.locals.error_msg = req.flash('error_msg');
-  // res.locals.error = req.flash('error');
-  res.locals.user = req.user || null;
-  // res.locals.username = req.username;
-  next();
-});
-
+// GlOBAL VARS
+// app.use(function(req, res, next) {
+//   // res.locals.success_msg = req.flash('success_msg');
+//   // res.locals.error_msg = req.flash('error_msg');
+//   // res.locals.error = req.flash('error');
+//   res.locals.user = req.user || null;
+//   // res.locals.username = req.username;
+//   next();
+// });
 
 // Start the server
 app.listen(3000, function() {
-  console.log("Magic happens on port 3000");
+  console.log('Magic happens on port 3000');
 });
