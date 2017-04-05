ae696f77e037aecd777e0133ad5fb28f52422916
AliWaleed97 <ali.sobh@student.guc.edu.eg>
2017-04-05 19:32:42 +0200

    dodo finished testing

--- source/controllers/offerController.js
+++ source/controllers/offerController.js
@@ -3,13 +3,13 @@ let Offer = require("../models/Offer");
 let offerController = {
 
   createOffer: function(req, res) { //posting new offer
-
+    const user = req.user;
     //making a new offer instance and saving it
     var newOffer = new Offer({
       //updated
       title: req.body.title,
       price: req.body.price,
-      sp_id: req.body.sp_id,
+      sp_id: user.id,
       field: req.body.field,
       capacity: req.body.capacity,
       description: req.body.description,
@@ -21,7 +21,7 @@ let offerController = {
     res.json(newOffer.capacity);
     console.log('new offer posted successfully');
 
-}
+  }
 
 
 };
