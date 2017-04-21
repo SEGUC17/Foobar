e085e7431beeb7080158982b1c0c93ef38b30d08
replose <ahmed.ahmed-elkady@student.guc.edu.eg>
2017-04-21 13:37:02 +0200

    comments to reviews

--- vue/src/components/ServiceProviderProfile.vue
+++ vue/src/components/ServiceProviderProfile.vue
@@ -486,6 +486,14 @@ a:active {
   -webkit-transition:width 0.5s ease-in-out;
 }
 
+#myInput1		{
+  position: inline;
+
+
+  width:90%;
+  -webkit-transition:width 0.3s ease-in-out;
+
+}
 
 #makan1 {
     padding-left: 7px;
@@ -494,6 +502,87 @@ a:active {
   padding-left: 30px;
 
 }
+  @import "//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css";
+  .box > .icon {
+      text-align:
+      center;
+      position:
+      relative;
+      }
+  .box > .icon > .image {
+      position: relative;
+      z-index: 2;
+      margin: auto;
+      width: 88px;
+      height: 88px;
+      border: 8px solid white;
+      line-height: 88px;
+      border-radius: 50%;
+      background: #f0f0f0;
+      vertical-align: middle;
+      }
+  .box > .icon > .image > i {
+      font-size: 36px !important;
+      color: #fff !important;
+      }
+  .box > .icon:hover > .image > i {
+      color: white !important;
+      }
+  .box > .icon > .info {
+      margin-top: -24px;
+      background: rgba(0, 0, 0, 0.04);
+      border: 1px solid #e0e0e0;
+      padding: 15px 0 10px 0;
+      }
+  .box > .icon > .info > h3.title {
+      font-family: sans-serif !important;
+      font-size: 16px;
+      color: #222;
+      font-weight: 500;
+      }
+  .box > .icon > .info > p {
+      font-family: sans-serif !important;
+      font-size: 13px;
+      color: #666;
+      line-height: 1.5em;
+      margin: 20px;
+      }
+  .box > .icon:hover > .info > h3.title, .box > .icon:hover > .info > p, .box > .icon:hover > .info > .more > a {
+      color: #222;
+      }
+  .box > .icon > .info > .more a {
+      font-family: sans-serif !important;
+      font-size: 12px;
+      color: #222;
+      line-height: 12px;
+      text-transform: uppercase;
+      text-decoration: none;
+      }
+  .box > .icon:hover > .info > .more > a {
+      color: #fff;
+      padding: 6px 8px;
+      background-color: #63B76C;
+      }
+  .box .space {
+      height: 30px;
+      }
+
+  .modal-header-primary {
+      color:#fff;
+      padding:9px 15px;
+      border-bottom:1px solid #eee;
+      background-color: #428bca;
+      -webkit-border-top-left-radius: 5px;
+      -webkit-border-top-right-radius: 5px;
+      -moz-border-radius-topleft: 5px;
+      -moz-border-radius-topright: 5px;
+       border-top-left-radius: 5px;
+       border-top-right-radius: 5px;
+  }
+
+
+
+
 </style>
 
 <template>
@@ -635,15 +724,6 @@ a:active {
 
 
 
-
-
-
-
-
-
-
-
-
             </div>
           </div>
         </div>
@@ -651,27 +731,49 @@ a:active {
       </div>
 
       </div>		</div> </div>
-      <!-- Modal -->
-      <div class="modal fade" id="primary" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-          <div class="modal-dialog">
-              <div class="modal-content">
-                  <div class="modal-header modal-header-primary">
-                      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                  </div>
-                  <div class="modal-body">
+      <div class="container">
+    <div class="row">
+        <!-- Boxes de Acoes -->
+        <div class="col-xs-12 col-sm-6 col-lg-4">
+			<div class="box">
+				<div class="icon">
 
-                    <div v-for =" p in pastComments" >
+    <!-- Modal -->
+    <div class="modal fade" id="primary" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header modal-header-primary">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                    <h3     align="left">  <div v-for="comment in pastComments">
+                          {{comment.commenter_id.name}}: {{comment.content}}
+                          <br>
+                      </div></h3>
 
-                      <h3 class="title">
-                      <p class="info">  <strong>{{p.commenter_id.name}} </strong> {{p.content}}
-                      </p></h3>
-                    <div class="container"></div></div>
-                   </div>
-                  <div class="modal-footer">
-                  </div>
-              </div><!-- /.modal-content -->
-          </div><!-- /.modal-dialog -->
-      </div><!-- /.modal -->
+                </div>
+                <div class="modal-body">
+                  <div class="row">
+
+                <input style="height:30px;font-size:10pt"class="form-control input-lg" id="myInput1" placeholder="Write a comment" type="text"v-model="comment">
+
+                    </input>
+                </div>                </div>
+
+                <div class="modal-footer">
+
+
+                    <button class="btn btn-primary add_field_button" style="margin-bottom:20px;" v-on:click="addComment" >Post comment</button>
+
+                                 </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+    <!-- Modal -->
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- /Boxes de Acoes -->
+	</div>
   <div class="row">
     <div class="col-md-12">
             <input style="height:50px;font-size:10pt"class="form-control input-lg" id="myInput" placeholder="Write a review" type="text"v-model="review"><br /><br /><br />
@@ -729,7 +831,7 @@ a:active {
       review:'',
       rating:0,
       reviews:[],
-      comment:[],
+      comment:'',
       pastComments:[],
       reviewid:''
      }
@@ -791,10 +893,11 @@ console.log(response.data.data.reviews)
          this.pastComments =response.data.data.comments
          })
        },
-       addComment: function(review_id){
-         this.$http.post('http://localhost:3000/api/users/comments/create', {"content":this.comment[index],"review_id":review_id}, {headers : {'jwt-token' : localStorage.getItem('id_token')}}).then(data => {
+       addComment: function(){
+         this.$http.post('http://localhost:3000/api/users/comments/create', {"content":this.comment,"review_id":this.reviewid}, {headers : {'jwt-token' : localStorage.getItem('id_token')}}).then(data => {
                console.log('success');
-              this.viewComments(review_id);
+              this.viewComments(this.reviewid);
+              this.comment =''
                        });
        }
 
