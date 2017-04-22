e747b92a8559bb38453b99b9b48ce02640eb7986
Ahmedash11 <dodo11-10@hotmail.com>
2017-04-22 20:56:19 +0200

    Pagination in view announcements

--- vue/src/components/Announcements.vue
+++ vue/src/components/Announcements.vue
@@ -5,7 +5,7 @@
     <center>
       <h2>Announcements</h2></center>
       <br><br><br>
-     <div v-for =" announcement in announcements" class="row-2">
+     <div v-for =" announcement in announcementsInPage" class="row-2">
         <div class="container" align="center">
           <h3 align="left">{{announcement.title}}</h3>
             
@@ -13,12 +13,14 @@
             <h3>Announcer Name: {{announcement.announcer_id.name}}</h3>
             <h3>Announcer Email: {{announcement.announcer_id.email}}</h3>
           <h2>{{announcement.type}}</h2>
+
+          
             
       </div>
         </div>
     
   
-
+          <li v-for="n in numberOfPages"><a @click="changePage(n)">{{n}}</a></li>
 
   </div>
 </template>
@@ -27,7 +29,11 @@ export default {
   name: 'announcements',
   data () {
     return {
-      announcements:[]
+      announcements:[],
+      announcementsInPage:[],
+      numberOfPages: 0 , 
+      perPage: 3 
+
     }
   },
 created(){
@@ -37,7 +43,25 @@ methods:{
     getAllAnnouncements: function () {
       this.$http.get('http://localhost:3000/api/announcements/view').then(response => {
         this.announcements=response.data.data.announcements
+        console.log(this.announcements);
+        this.numberOfPages=Math.ceil(this.announcements.length/this.perPage);
+        for(var i = 0 ; i<this.perPage && i<this.announcements.length ; i++){
+          this.announcementsInPage.push(this.announcements[i]);
+
+        }
+        console.log(this.announcementsInPage)
+
       })
-    }}
+    },
+    changePage: function(number){
+      var lastPost = number * this.perPage
+      var firstPost = (number*this.perPage)-this.perPage
+      this.announcementsInPage = []
+      
+      for(var i = firstPost ; i<lastPost && i<this.announcements.length ; i++){
+       this.announcementsInPage.push(this.announcements[i]);
+      }
+    }
+  }
 }
 </script>
