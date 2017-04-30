211b187f0629d2043899b2d0bd77d1eeca735aa8
bassemawhoob <bassem.mawhoob@live.com>
2017-04-30 10:31:26 +0200

    Reset PW

--- vue/src/AppNav.vue
+++ vue/src/AppNav.vue
@@ -71,7 +71,10 @@
                       <div class="row">
                         <div class="col-lg-12">
                           <div class="text-center">
-                            <a href="http://phpoll.com/recover" tabindex="5" class="forgot-password">Forgot Password?</a>
+                     <div class="form-group">
+                      <input type="text"  tabindex="1" v-model="resetPWEmail" class="form-control efc" placeholder="Email" value="">
+                    </div>
+                              <a @click.prevent="resetPW" tabindex="5" class="forgot-password">Forgot Password?</a>
                           </div>
                         </div>
                       </div>
